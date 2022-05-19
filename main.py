9fbd94220b3b245f364c51197f92a752838d1aff
Xilas <dahepix1@gmail.com>
2022-05-19 16:23:40 +0200

    small changes and fixes

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from main_classes.item import Item
 from main_classes.game import Game
+from main_classes.level import Level
 
 g = Game()
+l1 = Level(g)
 
-player = Item(game = g, type = "PLAYER", name = "DoDo")
+player = Item(game = g, level = l1, type = "PLAYER", name = "DoDo")
 
-hat = Item(game = g, type = "HAT", name = "Tattered Cowl", parent = player)
-gloves = Item(game = g, type = "GLOVE", name = "Worm Glove", parent = player)
-boots = Item(game = g, type = "BOOT", name = "Worm Boots", parent = player)
-sword = Item(game = g, type = "SWORD", name = "Rusty Sword", parent = player)
-shield = Item(game = g, type = "SHIELD", name = "Busted Shield", parent = player)
+hat = Item(game = g, level = l1, type = "HAT", name = "Tattered Cowl", parent = player)
+gloves = Item(game = g, level = l1, type = "GLOVE", name = "Worm Glove", parent = player)
+boots = Item(game = g, level = l1, type = "BOOT", name = "Worm Boots", parent = player)
+sword = Item(game = g, level = l1, type = "SWORD", name = "Rusty Sword", parent = player)
+shield = Item(game = g, level = l1, type = "SHIELD", name = "Busted Shield", parent = player)
 
 
-enemy = Item(game = g,type = "ENEMY", name = "Bob")
+enemy = Item(game = g,level = l1, type = "ENEMY", name = "Bob")
 
 
 g.running = True
