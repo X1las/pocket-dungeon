c35c41f41782d40e345d0d57292434e4c552723c
Xilas <dahepix1@gmail.com>
2022-05-22 12:50:01 +0200

    documentation

---
+++ data/enemytypes.py
@@ -0,0 +1,7 @@
+# Enemy Types docuent, includes their NPC name, population cap, modules and additions
+
+BRAWLER = {
+    "name"          : "Brawler",
+    "population"    : 6000,
+    "modules"       : 2
+}
---
+++ data/roomtypes.py
@@ -0,0 +1,11 @@
+# Room types document, includes their name, module sizes and other effects or parameters
+
+FOUNTAIN = {
+    "name"  : "Fountain",
+    "size"  : 1
+}
+
+STANDARD = {
+    "name"  : "Standard",
+    "size"  : 1
+}
