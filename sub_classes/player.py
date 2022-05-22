423ae5e22de5225ccb289f392453390f26b540f4
Xilas <dahepix1@gmail.com>
2022-05-22 15:14:19 +0200

    made player sub-class

---
+++ sub_classes/player.py
@@ -0,0 +1,11 @@
+from main_classes.game import Game
+from main_classes.item import Item
+
+
+class Player(Item):
+    
+    def __init__(self,game:Game):
+        self.game = game
+    
+    def playerController():
+        pass
